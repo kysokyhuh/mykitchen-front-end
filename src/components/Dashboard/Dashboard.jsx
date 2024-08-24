40902c0ddff0041b8df7ba63aeff6d9c18184089
gabtongol <geybtongol@gmail.com>
2024-08-23 20:12:53 -0400

    tailwind installation

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -6,7 +6,7 @@ const Dashboard = ({}) => {
   return (
     <main>
       <h1>Welcome, {user.username}</h1>
-      <p>
+      <p className="">
         This is the dashboard page where you, and only you, can see a dashboard
         of all of your things.
       </p>

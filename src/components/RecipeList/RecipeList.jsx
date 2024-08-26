25e20a276a6def97f9fcc349d8d91009f09d3349
gabtongol <geybtongol@gmail.com>
2024-08-26 17:30:19 -0400

    renders the public recipes in the landing page

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -1,16 +1,26 @@
 import { AuthedUserContext } from '../../App';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+
 
 const RecipeList = (props) => {
   const user = useContext(AuthedUserContext);
+  const [publicRecipes, setPublicRecipes] = useState([]) 
+
+
+  useEffect(() => {
+       setPublicRecipes(props.recipes.filter(recipe => recipe.isPrivate === false));
+  }, [props.recipes])
+
+  console.log('public: ', publicRecipes);
   return (
     <div>
-      {props.recipes.map((recipe) => (
-        
-        <Link key={recipe._id} to={`/recipes/${recipe._id}`}>
-            <h2>{recipe.name}</h2>
-        </Link>
+      {publicRecipes.map((recipe) => (
+
+          <Link key={recipe._id} to={`/recipes/${recipe._id}`}>
+              <h2>{recipe.name}</h2>
+          </Link>
 
       ))}
       

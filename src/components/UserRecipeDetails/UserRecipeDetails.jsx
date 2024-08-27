3b785fbcd0e653d3c61b4f71b6dc0718fe98f7dd
gabtongol <geybtongol@gmail.com>
2024-08-27 12:01:14 -0400

    render user's recipes

--- src/components/UserRecipeDetails/UserRecipeDetails.jsx
+++ src/components/UserRecipeDetails/UserRecipeDetails.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { AuthedUserContext } from '../../App';
+import { useParams, Link } from 'react-router-dom'
+import { useEffect, useState, useContext } from 'react';
+
+import * as recipeService from '../../services/recipeService'
 
 const UserRecipeDetails = () => {
+    const { userId } = useParams()
+    const [recipes, setRecipes] = useState([])
+    const user = useContext(AuthedUserContext)
+
+
+    useEffect(() => {
+        const fetchUserRecipes = async () => {
+            const recipeData = await recipeService.showUserRecipes(userId)
+            console.log('User recipe', recipeData)
+            setRecipes(recipeData)
+        }
+        fetchUserRecipes()
+    }, [userId])
+
+    if (!recipes) return <main>Loading...</main>
+
   return (
     <div>
-        hello this is user recipes
+        <p>Hello {user.username}!</p>
+        <p>Here are your recipes</p>
+
+        {recipes.map((recipe) => (
+            <div key={recipe._id}>
+                <Link to={`/recipes/${recipe._id}`}>{recipe.name}</Link>
+            </div>
+        ))}
+        
     </div>
   )
 }

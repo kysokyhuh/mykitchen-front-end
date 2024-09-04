e6bd74a8237828e504817fafacfbfc7f27ac4cac
gabtongol <geybtongol@gmail.com>
2024-09-03 20:54:11 -0400

    home animation

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,7 +1,8 @@
 import homeImg from '/Home.svg'
+import { TypeAnimation } from 'react-type-animation';
 
 const Landing = () => {
-
+  const text ="this is a test"
   // place content for homepage design
   return (
     <main className=" z-0 bg-cream flex items-center justify-center min-h-screen relative ">
@@ -9,9 +10,28 @@ const Landing = () => {
       <div className="absolute mx-auto font-literata text-center top-64 md:top-48 text-darksage z-20">
         <p className="text-5xl md:text-8xl font-bold">Kusina Ku</p>
         <p className=" text-2xl md:text-4xl italic">"My Kitchen"</p>
+       
+       
+        <div className=' mt-1 md:mt-9'>
+          <TypeAnimation
+              sequence={[
+                'Share Your Recipes',
+                1000, 
+                'Explore New Recipes',
+                1000,
+                'Save Your Favorite Recipes',
+                1000,
+                'Join the Community!',
+                1000
+              ]}
+              wrapper="span"
+              speed={50}
+              className=' mx-auto text-lg md:text-2xl'
+              repeat={Infinity}
+            />
+        </div>
       </div>
 
-  
         <img 
           src={homeImg}
           alt="homebg" 

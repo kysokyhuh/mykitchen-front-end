67e92dd3a61f63b03920fde3ddf8d11aeb13c5c6
gabtongol <geybtongol@gmail.com>
2024-09-03 21:07:44 -0400

    text animation greetings

--- src/components/Greeting/Greeting.jsx
+++ src/components/Greeting/Greeting.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
 import { useParams } from 'react-router-dom';
+import { TypeAnimation } from 'react-type-animation';
+
 const Greeting = (props) => {
 
   const { recipeId } = useParams()
@@ -9,39 +11,55 @@ const Greeting = (props) => {
     greeting = (
       <>
         <p className=' text-redorange'>Hey {props.user.username}!</p>
-        <p className=' text-lg md:text-2xl '>What are you craving for today?</p> 
+        <TypeAnimation 
+          sequence={["What are you craving for today?"]}
+          className='text-lg md:text-2xl'
+        />
+
       </>
     )
   } else if (recipeId){
     greeting = (
       <>
         <p className=' text-redorange'>Hey {props.user.username}!</p>
-        <p className=' text-lg md:text-2xl '>Ready to cook something good?</p> 
+        <TypeAnimation 
+          sequence={["Ready to cook something good?"]}
+          className='text-lg md:text-2xl'
+        />
       </>
     )
   } else if (location.pathname === '/recipes/new' ){
     greeting = (
       <>
         <p className=' text-redorange'>Hey {props.user.username}!</p>
-        <p className=' text-lg md:text-xl '>Ready to share your culinary masterpiece?</p> 
+        <TypeAnimation 
+          sequence={["Ready to share your culinary masterpiece?"]}
+          className='text-lg md:text-2xl'
+        />
       </>
     )
   } else if (location.pathname.endsWith('/favorites')){
     greeting = (
       <>
         <p className=' text-redorange'>Hey {props.user.username}!</p>
-        <p className=' text-lg md:text-2xl '>Your collection of saved recipes awaits!</p> 
+        <TypeAnimation 
+          sequence={["Your collection of saved recipes awaits!"]}
+          className='text-lg md:text-2xl'
+        />
+  
       </>
     )
   } else if (location.pathname.startsWith('/recipes/user/')){
     greeting = (
       <>
         <p className=' text-redorange'>Hey {props.user.username}!</p>
-        <p className=' text-lg md:text-xl '>Look at the delicious recipes you've shared!</p> 
+        <TypeAnimation 
+          sequence={["Here are the delicious recipes you've shared!"]}
+          className='text-base md:text-xl'
+        />
       </>
     )
   }
-
 
   return (
     <div className=' ml-10 mb-9 mt-24 font-literata italic md:text-4xl text-2xl font-semibold'>

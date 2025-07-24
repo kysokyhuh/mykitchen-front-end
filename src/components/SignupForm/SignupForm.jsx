526df71f44fa1f309b76f25c08f8577e06883c50
kysokyhuh <130388727+kysokyhuh@users.noreply.github.com>
2025-07-24 16:18:49 +0800

    minor changes

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -118,7 +118,7 @@ const SignupForm = (props) => {
 
           {/* Security Question 1 */}
           <div>
-            <label className=' mb-3 font-semibold text-lg' htmlFor="securityQuestion1">Security Question 1:</label>
+            <label className=' mb-3 font-semibold text-lg' htmlFor="securityQuestion1">Question 1:</label>
             <select
               className=' h-9 px-6 mt-1 mb-1 border border-sage rounded-lg'
               id="securityQuestion1"
@@ -146,7 +146,7 @@ const SignupForm = (props) => {
 
           {/* Security Question 2 */}
           <div>
-            <label className=' mb-3 font-semibold text-lg' htmlFor="securityQuestion2">Security Question 2:</label>
+            <label className=' mb-3 font-semibold text-lg' htmlFor="securityQuestion2">Question 2:</label>
             <select
               className=' h-9 px-6 mt-1 mb-1 border border-sage rounded-lg'
               id="securityQuestion2"
